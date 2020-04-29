7babb9ef36dd61bbd422daab6e629cdba9d722a1
MondaleFelix <mondalefelix@yahoo.com>
2020-04-28 21:57:52 -0700

    added album list

--- music/urls.py
+++ music/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.musicians, name='home'),
+	path('musician_detail/<int:id>', views.musician_detail, name='musician_detail'),
+
 ]
--- music/views.py
+++ music/views.py
@@ -13,3 +13,13 @@ def musicians(request):
 		'musicians' : Musician.objects.all()
 	}
 	return render(request, 'music/musician_list.html', context = context)
+
+
+def musician_detail(request, id):
+	name = Musician.objects.get(id=id)
+
+	context = {
+		'musician': Musician.objects.get(id=id),
+		"albums_list": Album.objects.filter(artist=name)
+	}
+	return render(request, 'music/musician_detail.html',context = context)
